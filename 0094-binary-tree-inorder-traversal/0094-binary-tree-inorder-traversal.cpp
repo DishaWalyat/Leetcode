2a30e373948adfff0e4a9f039a20d2dd543e80ae
DishaWalyat <dishawalyat@gmail.com>
2026-05-24 13:33:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (65.34%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,25 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+ // inorder ka matlab left root right 
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
-        stack<TreeNode*> st;
-        TreeNode* curr = root;
-
-        while (curr != NULL || !st.empty()) {
-            
-            while (curr != NULL) {
-                st.push(curr);
-                curr = curr->left;
-            }
-
-            curr = st.top();
-            st.pop();
-            res.push_back(curr->val);
-
-            
-            curr = curr->right;
+    void inorder(TreeNode * root, vector<int> &ans){
+        if ( root == NULL){
+            return; 
         }
+        inorder( root-> left, ans);
+        ans.push_back(root->val);
+        inorder( root-> right, ans);
 
-        return res;
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector <int> ans;
+        inorder(root,ans);
+        return ans;
+        
     }
 };
