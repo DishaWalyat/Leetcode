ccdec6ddcd6e885c34da8d76817de828db57653d
DishaWalyat <dishawalyat@gmail.com>
2026-06-03 13:23:52 +0530

    Time: 0 ms (100%), Space: 20.9 MB (29.5%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,53 +1,34 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        // optimal space complexity solution 
+        vector<int> row(matrix.size(), 0);
+        vector<int> col(matrix[0].size(), 0);
 
-        bool isrow0 = false;
-        bool iscol0 = false;
-
-        for( int i= 0 ; i < matrix.size(); i ++){
-            if( matrix[i][0] == 0) iscol0= true;
-        }
-        for( int j = 0 ; j < matrix[0].size(); j ++){
-                if( matrix[0][j]==0) isrow0= true;
-        }
-        
-
-        for( int i=1 ; i < matrix.size(); i ++){
-            for( int j = 1 ; j < matrix[0].size(); j ++){
-                if (matrix[i][j]==0){
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
+        for( int i=0; i <matrix.size(); i ++){
+            for( int j =0; j < matrix[0].size(); j++){
+                if ( matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
-        for( int i=1 ; i < matrix.size(); i ++){
-            for( int j = 1 ; j < matrix[0].size(); j ++){
-                if ( matrix[0][j]==0 || matrix[i][0]==0){
-                    matrix[i][j]=0;
-
+        for( int i = 0 ; i < row.size(); i ++){
+            if(row[i]== 1){
+                for( int j = 0; j < matrix[0].size(); j++){
+                    matrix[i][j] = 0;
                 }
-                
+            }
+        }
+
+        for( int i = 0 ; i < col.size(); i ++){
+            if(col[i]== 1){
+                for( int j = 0; j < matrix.size(); j++){
+                    matrix[j][i] = 0;
+                }
             }
         }
 
 
-        if (isrow0){
-
-            for( int j=0; j < matrix[0].size(); j++){
-                matrix[0][j]=0;
-            }
-        }
-
-        if (iscol0){
-
-            for( int i=0; i < matrix.size(); i++){
-                matrix[i][0]=0;
-            }
-        }
-
-      
     }
 };
