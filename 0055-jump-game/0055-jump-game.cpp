1f20e3bc1a19200e58583fa4777f4cb32b9dbc6a
DishaWalyat <dishawalyat@gmail.com>
2026-06-05 00:42:04 +0530

    Time: 0 ms (100%), Space: 52.2 MB (82.57%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,6 +4,7 @@ public:
         int maxReach = 0;
 
         for (int i = 0; i < nums.size(); i++) {
+            
             if (i > maxReach) return false;
 
             maxReach = max(maxReach, i + nums[i]);
