91b6e2bbf8191f30d170a053b5cc63b6e1a5a6ea
DishaWalyat <dishawalyat@gmail.com>
2026-06-08 00:57:33 +0530

    Time: 20 ms (29.63%), Space: 47.2 MB (58.38%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
+        int ans = 0;
         int n = arr.size();
-        long long MOD = 1e9 + 7;
-        
-        vector<int> left(n), right(n);
+        int mod = 1e9 + 7;
+
+        vector<int> nextSmaller(n);
+        vector<int> prevSmaller(n);
         stack<int> st;
 
-        // Step 1: Previous Smaller Element (PSE) nikalna
-        for (int i = 0; i < n; i++) {
+        for (int i = n - 1; i >= 0; i--) {
             while (!st.empty() && arr[st.top()] >= arr[i]) {
                 st.pop();
             }
-            // Agar stack khali hai toh left boundary -1, nahi toh st.top()
-            left[i] = st.empty() ? -1 : st.top();
+
+            nextSmaller[i] = st.empty() ? n : st.top();
+
             st.push(i);
         }
 
-        // Stack ko khali karo doosre pass ke liye
-        while (!st.empty()) st.pop();
+        while (!st.empty())
+            st.pop();
 
-        // Step 2: Next Smaller Element (NSE) nikalna
-        for (int i = n - 1; i >= 0; i--) {
-            // Yahan strictly '>' use kiya duplicates se bachne ke liye
+        for (int i = 0; i < n; i++) {
             while (!st.empty() && arr[st.top()] > arr[i]) {
                 st.pop();
             }
-            // Agar stack khali hai toh right boundary 'n', nahi toh st.top()
-            right[i] = st.empty() ? n : st.top();
+
+            prevSmaller[i] = st.empty() ? -1 : st.top();
+
             st.push(i);
         }
 
-        // Step 3: Total Sum Calculate karna
-        long long total_sum = 0;
         for (int i = 0; i < n; i++) {
-            long long L = i - left[i];
-            long long R = right[i] - i;
-            
-            long long total_subarrays = (L * R) % MOD;
-            long long contribution = (total_subarrays * arr[i]) % MOD;
-            
-            total_sum = (total_sum + contribution) % MOD;
+            long long next = nextSmaller[i] - i;
+            long long prev = i - prevSmaller[i];
+
+            long long contrib = (arr[i] * next % mod) * prev % mod;
+
+            ans = (ans + contrib) % mod;
         }
 
-        return total_sum;
+        return ans;
     }
 };
