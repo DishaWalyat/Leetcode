463eef3d835525b6a12917576313804f646c80ac
DishaWalyat <dishawalyat@gmail.com>
2026-06-23 17:42:38 +0530

    Time: 44 ms (29.38%), Space: 32.4 MB (40.97%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-    if (hand.size() % groupSize != 0) return false;
-
-    map<int, int> freq;
-    for (int x : hand) freq[x]++;
-
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-    for (auto& [num, _] : freq)
-        minHeap.push(num);
-
-    while (!minHeap.empty()) {
-        int start = minHeap.top();
-
-        for (int i = 0; i < groupSize; i++) {
-            int curr = start + i;
-
-            if (freq[curr] == 0)
-                return false;
-
-            freq[curr]--;
-
-            if (freq[curr] == 0) {
-                if (curr != minHeap.top())
-                    return false;
-                minHeap.pop();
-            }
+        int n= hand.size();
+        //agar grps mein bant hi nhi skte to directly false ret 
+        if(n%groupSize){
+            return false;
         }
-    }
+        //now we will make a ordered map so that seq mrin hi inc fasion mein simulation kar pae 
+        map<int, int> mp; // number and the freq
+        for(int &num: hand){
+            mp[num]++;
+        }// in order to store the freq of each element 
+        while(!mp.empty()){
+            int curr= mp.begin()->first;// sec would be the freq 
+            for(int i=0;i<groupSize;i++){
+                if(mp[curr+i]==0){
+                    //i zero hoga to curr ko hi point krega..to hum ek grp ke sare element nikal denge
+                    return false;
+                }
+                //else.. vo hai to use krke freq minus minus
+                mp[curr+i]--;
+                if(mp[curr+i]<1){
+                    //means vo khtm hogya..to htadenge
+                    mp.erase(curr+i);
+                }
+            }
 
-    return true;
-}
+        }
+        return true;
+
+    }
 };
