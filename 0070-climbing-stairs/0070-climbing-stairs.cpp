f13be5c919eccd53e435a66dfe5334b0048c3b6f
DishaWalyat <dishawalyat@gmail.com>
2026-07-04 21:07:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
+    int solve(int n, vector<int>& dp){
+        if(n<=2) return n;
+        if(dp[n]!= -1) return dp[n];
+
+        return dp[n]= solve(n-1,dp)+ solve(n-2,dp);
+
+    }
     int climbStairs(int n) {
         // if(n==1) return 1;
         // if(n==2) return 2;      TLEE
         // return climbStairs(n-1)+climbStairs(n-2); 
 
-        if( n<=2) return n;
+        // if( n<=2) return n;
 
-        int c;
-        int a=1;  
-        int b=2;
-        for(int i=2; i<n; i++ ){
-            c=a+b;
-            a=b;
-            b=c;
-        } 
-        return c;
+        // int c;
+        // int a=1;  
+        // int b=2;
+        // for(int i=2; i<n; i++ ){    constant space complexity 
+        //     c=a+b;
+        //     a=b;
+        //     b=c;
+        // } 
+        // return c;
+
+        if( n<= 2) return n;
+
+        vector<int> dp(n+1,-1);
+
+        return solve(n,dp);
 
     }
 };
