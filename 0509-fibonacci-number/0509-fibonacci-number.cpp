5075ba9f6727a56b07f4b9fcbbe7f178ba79e2e4
DishaWalyat <dishawalyat@gmail.com>
2026-07-04 20:37:05 +0530

    Time: 0 ms (100%), Space: 8.4 MB (10.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    int solve(int n, vector<int>& dp){
+        if( n<=1 ) return n;
+
+        if( dp[n]!=-1) return dp[n];
+
+        return dp[n]= solve(n-1,dp) + solve(n-2,dp);
+    }
+
     int fib(int n) {
         // recursion mai bahut time waste hai kyunki already jinka resuly nikaal chuke hai woh baar baar karna padhra hai instead isko dp se karengay
         // if (n==0) return n;
@@ -11,19 +19,25 @@ public:
         //dp approach
         //linear space complexity
 
+        // if( n<=1 ) return n;
+
+        // int c;
+        // int a=0; 
+        // int b=1;
+
+        // for( int i=1; i <n; i++){
+        //     c=a+b;
+        //     a=b;
+        //     b=c;
+        // }
+
+        // return c;
+
+        // dp 1D array solution
         if( n<=1 ) return n;
+        vector<int> dp(n+1,-1);
 
-        int c;
-        int a=0; 
-        int b=1;
-
-        for( int i=1; i <n; i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-
-        return c;
-        
+        return solve(n,dp);
+    
     }
 };
