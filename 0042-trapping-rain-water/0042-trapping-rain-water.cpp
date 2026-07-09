890a2cbd516193631315fa81c152ca7bd1660a29
DishaWalyat <dishawalyat@gmail.com>
2026-07-09 21:13:11 +0530

    Time: 3 ms (16.85%), Space: 27.8 MB (6.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack<int> st; // Indices store karega (Decreasing Order me)
-        int total_water = 0;
         int n = height.size();
 
-        for (int i = 0; i < n; i++) {
-            // Jab bhi koi badi building mile jo stack ke top se unchi ho
-            while (!st.empty() && height[i] > height[st.top()]) {
-                
-                int bottom_idx = st.top(); // Gaddhe ka floor
-                st.pop(); 
+        vector<int> prev(n), next(n), mini(n);
 
-                // Agar left me koi building hi nahi bachi, toh paani ruk nahi sakta
-                if (st.empty()) break;
-
-                int left_idx = st.top(); // Left deewar
-                
-                // Horizontal calculations
-                int width = i - left_idx - 1;
-                int bounded_height = min(height[left_idx], height[i]) - height[bottom_idx];
-
-                total_water += width * bounded_height;
-            }
-            
-            // Agar building choti ya barabar hai, toh bas chupchaap index push karo
-            st.push(i);
+        // prev greatest
+        prev[0] = height[0];
+        for(int i = 1; i < n; i++) {
+            prev[i] = max(prev[i-1], height[i]);
         }
-        return total_water;
+
+        // next greatest
+        next[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            next[i] = max(next[i+1], height[i]);
+        }
+
+        // min boundary
+        for(int i = 0; i < n; i++) {
+            mini[i] = min(prev[i], next[i]);
+        }
+
+        // water calculation
+        int water = 0;
+        for(int i = 0; i < n; i++) {
+            if(mini[i] > height[i])
+                water += mini[i] - height[i];
+        }
+
+        return water;
     }
 };
