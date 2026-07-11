d865b7b4fa28e43c55114e04e2f680f4aec7407f
DishaWalyat <dishawalyat@gmail.com>
2026-07-11 23:56:47 +0530

    Time: 19 ms (45.7%), Space: 24.8 MB (88.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    // Helper function that reconstructs the word in sorted order using Counting Sort
+    string generate(string& word) {
+        int arr[26] = {0};
         
-        unordered_map<string,vector<string>>mp;
-
-        for (string s : strs) {
-
-            string temp = s;
-
-            sort(temp.begin(), temp.end());
-
-            mp[temp].push_back(s);
+        for(char &ch : word) {
+            arr[ch - 'a']++;
         }
-        vector<vector<string>> ans;
-
-        for (auto it : mp) {
-
-            ans.push_back(it.second);
+        
+        string new_word = "";
+        
+        for(int i = 0; i < 26; i++) {
+            int freq = arr[i];
+            
+            if(freq > 0) {
+                // Appends the character (i + 'a') repeated 'freq' times
+                new_word += string(freq, i + 'a');
+            }
         }
-
-        return ans;
-
-
+        
+        return new_word;
+    }
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        
+        unordered_map<string, vector<string>> mp;
+        
+        for(int i = 0; i < n; i++) {
+            string word = strs[i];
+            // 1. Generate the sorted key for the current word
+            string key = generate(word);
+            // 2. Group the original word under its key
+            mp[key].push_back(word);
+        }
+        
+        // 3. Move the grouped values from the map into the final result matrix
+        vector<vector<string>> result;
+        for(auto& pair : mp) {
+            result.push_back(move(pair.second));
+        }
+        
+        return result;
     }
 };
