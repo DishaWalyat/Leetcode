ba08560af2a2f6b57500db4311285cabafd8eb27
DishaWalyat <dishawalyat@gmail.com>
2026-08-06 20:54:38 +0530

    Time: 327 ms (45.09%), Space: 302.4 MB (46.39%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -38,3 +38,46 @@ public:
         return dfs(source, destination, adj, vis);
     }
 };
+
+// class Solution {
+// public:
+//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
+//         //adj list
+//         vector<vector<int>> adj(n);
+//         for(int i = 0; i < edges.size(); i++){
+//             int u = edges[i][0];
+//             int v = edges[i][1];
+            
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);//cuzz graph is bi-directional
+//         }
+        
+//         //visited array
+//         vector<bool> visited(n, false);
+        
+//         //BFS
+//         queue<int> q;
+//         q.push(source);
+//         visited[source] = true;
+        
+//         while(!q.empty()) {
+//             int node = q.front();
+//             q.pop();
+//             //edgecase
+//             if(node== destination)
+//                 return true;
+            
+//             for(int i =0; i <adj[node].size(); i++){
+//                 int neighbour = adj[node][i];
+                
+//                 if(!visited[neighbour]){
+//                     visited[neighbour] =true;
+//                     q.push(neighbour);
+//                 }
+//             }
+//         }
+        
+//         return false;
+//     }
+// };
