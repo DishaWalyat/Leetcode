cfc5d11f28fb64f24b6036065fca70cff82fdf24
DishaWalyat <dishawalyat@gmail.com>
2026-08-08 01:06:16 +0530

    Time: 3 ms (10.02%), Space: 27.3 MB (32.03%) - LeetHub

---
+++ 3452-sum-of-good-numbers/3452-sum-of-good-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfGoodNumbers(vector<int>& nums, int k) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if ((i - k < 0 || nums[i] > nums[i - k]) &&
+                (i + k >= nums.size() || nums[i] > nums[i + k])) {
+                sum += nums[i];
+            }
+
+        }
+        return sum;
+    }
+};
