b4a3adfd12ffc685d5688e4786955ff5991eacf2
DishaWalyat <dishawalyat@gmail.com>
2026-08-11 21:12:53 +0530

    Time: 46 ms (43.21%), Space: 16.8 MB (41.98%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dp;
+    bool solve(int n){
+        if(n==0) return false;
+        if(dp[n]!=-1){
+            if(dp[n]==1) return true;
+            else return false;
+        }
+        for(int k=1;k*k<=n;k++){
+            if(solve(n-(k*k))==false) return dp[n]=true; // this was bob chance;
+        }
+        return dp[n]=false;
+    }
+    bool winnerSquareGame(int n) {
+        dp.assign(n+1,-1);
+        return solve(n);
+    }
+};
