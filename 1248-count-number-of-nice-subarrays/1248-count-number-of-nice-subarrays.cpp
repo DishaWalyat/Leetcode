a99874e181d1a3492951e650db3ceac34e9efa70
DishaWalyat <dishawalyat@gmail.com>
2026-08-12 16:01:33 +0530

    Time: 3 ms (89.21%), Space: 71.5 MB (46.58%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        mp[0] = 1;
+    int atMost(vector<int>& nums, int k) {
+    int left = 0;
+    int ans = 0;
+    int odd = 0;
 
-        int sum = 0, ans = 0;
+    for(int right = 0; right < nums.size(); right++) {
 
-        for(int x : nums) {
-            sum += x % 2;
+        if(nums[right] % 2 == 1)
+            odd++;
 
-            if(mp.count(sum - k))
-                ans += mp[sum - k];
+        while(odd > k) {
+            if(nums[left] % 2 == 1)
+                odd--;
 
-            mp[sum]++;
+            left++;
         }
 
-        return ans;
+        ans += right - left + 1;
+    }
+
+    return ans;
+}
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        // unordered_map<int,int> mp;
+        // mp[0] = 1;
+
+        // int sum = 0, ans = 0;
+
+        // for(int x : nums) {
+        //     sum += x % 2;
+
+        //     if(mp.count(sum - k))
+        //         ans += mp[sum - k];
+
+        //     mp[sum]++;
+        // }
+
+        // return ans;
+
+        // since we have to find exact number of nice elements, so we need to do the atmost trick
+
+         return atMost(nums, k) - atMost(nums, k - 1);
     }
 };
