40a321d51c0929216215f6760d5a75d5692a1274
DishaWalyat <dishawalyat@gmail.com>
2026-08-15 14:33:02 +0530

    Time: 29 ms (66.65%), Space: 92 MB (60.9%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,30 +1,23 @@
 class StockSpanner {
 public:
-    // pair of {price, span} -> Monotonic Decreasing Stack
-    stack<pair<int, int>> st; 
+
+    stack<pair<int, int>> st;
 
     StockSpanner() {
-        // Constructor khali chhod sakte hain
     }
-    
-    int next(int price) {
-        int span = 1; // Shuruat me har element ka khud ka span 1 hota hai
 
-        // Jab tak piche choti ya barabar prices hain, unka cumulative span chura lo
+    int next(int price) {
+
+        int span = 1;
+
         while (!st.empty() && st.top().first <= price) {
+
             span += st.top().second;
             st.pop();
         }
 
-        // Current price aur uska total calculated span stack me push karo
         st.push({price, span});
 
         return span;
     }
 };
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner* obj = new StockSpanner();
- * int param_1 = obj->next(price);
- */
